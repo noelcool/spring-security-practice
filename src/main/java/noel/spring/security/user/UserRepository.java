971e471c337f51e76d0d24e2cf6f90120da6cda0
noel <noelcoolblue@gmail.com>
2022-05-31 17:45:27 +0900

    Add - 5.2.4 노트 서비스 로직

---
+++ src/main/java/noel/spring/security/user/UserRepository.java
@@ -0,0 +1,8 @@
+package noel.spring.security.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String name);
+}
